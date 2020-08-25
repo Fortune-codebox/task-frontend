a145018d9f12c3cab490b746dfe40298542b6c19
Fortune Uzoma <izzyuzoma@gmail.com>
2020-08-25 07:33:09 -0400

    pushing updates

--- src/axios-tasks.js
+++ src/axios-tasks.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:  'http://localhost:8000/api/'
+    baseURL:  'https://task-api-1.herokuapp.com/api/'
 });
 
 export default instance;
